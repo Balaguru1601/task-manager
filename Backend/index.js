de398b729cccc1cc069771290c05a45845919a70
Balaguru <balagurusethuraman@gmail.com>
2023-11-19 20:37:13 +0530

    cors fix 2

--- Backend/index.js
+++ Backend/index.js
@@ -41,7 +41,7 @@ mongoose
 app.use(session(sessionConfig));
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://task-manager-kp9d.vercel.app/");
+	res.header("Access-Control-Allow-Origin", "https://task-manager-kp9d.vercel.app");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
